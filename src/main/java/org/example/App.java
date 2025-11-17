6291eb60820b1201ef4624b413cfddf072379978
CalculatorSpringboot <alfeev01@mail.ru>
2025-11-17 19:28:30 +0300

    Создание Класса Judge2

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,42 +7,44 @@ public class App {
         Athlete athlete1 = new Athlete("Борис", 22, 80, "Троеборье");
         Athlete athlete2 = new Athlete("Алексей", 20, 83, "Троеборье");
         Athlete athlete3 = new Athlete("Григорий", 23, 82, "Троеборье");
+        
+        Judge judge = new Judge("Миша", 12345, 20);
 
-        athlete1.setSquatAttempt(200.0, 1, true);
-        athlete1.setSquatAttempt(210.0, 2, true);
-        athlete1.setSquatAttempt(225.0, 3, false);
+        athlete1.setSquatAttempt(200.0, 1, judge.validateAttempt());
+        athlete1.setSquatAttempt(210.0, 2, judge.validateAttempt());
+        athlete1.setSquatAttempt(225.0, 3, judge.validateAttempt());
 
-        athlete1.setBenchPress(110.0, 1, true);
-        athlete1.setBenchPress(120.0, 2, true);
-        athlete1.setBenchPress(130.0, 3, true);
+        athlete1.setBenchPress(110.0, 1, judge.validateAttempt());
+        athlete1.setBenchPress(120.0, 2, judge.validateAttempt());
+        athlete1.setBenchPress(130.0, 3, judge.validateAttempt());
 
-        athlete1.setDeadlift(170.0, 1, true);
-        athlete1.setDeadlift(185.0, 2, true);
-        athlete1.setDeadlift(200.0, 3, false);
+        athlete1.setDeadlift(170.0, 1, judge.validateAttempt());
+        athlete1.setDeadlift(185.0, 2, judge.validateAttempt());
+        athlete1.setDeadlift(200.0, 3, judge.validateAttempt());
 
-        athlete2.setSquatAttempt(220.0, 1, true);
-        athlete2.setSquatAttempt(230.0, 2, true);
-        athlete2.setSquatAttempt(245.0, 3, false);
+        athlete2.setSquatAttempt(220.0, 1, judge.validateAttempt());
+        athlete2.setSquatAttempt(230.0, 2, judge.validateAttempt());
+        athlete2.setSquatAttempt(245.0, 3, judge.validateAttempt());
 
-        athlete2.setBenchPress(115.0, 1, true);
-        athlete2.setBenchPress(125.0, 2, true);
-        athlete2.setBenchPress(135.0, 3, true);
+        athlete2.setBenchPress(115.0, 1, judge.validateAttempt());
+        athlete2.setBenchPress(125.0, 2, judge.validateAttempt());
+        athlete2.setBenchPress(135.0, 3, judge.validateAttempt());
 
-        athlete2.setDeadlift(180.0, 1, true);
-        athlete2.setDeadlift(195.0, 2, true);
-        athlete2.setDeadlift(210.0, 3, false);
+        athlete2.setDeadlift(180.0, 1, judge.validateAttempt());
+        athlete2.setDeadlift(195.0, 2, judge.validateAttempt());
+        athlete2.setDeadlift(210.0, 3, judge.validateAttempt());
 
-        athlete3.setSquatAttempt(190.0, 1, true);
-        athlete3.setSquatAttempt(205.0, 2, true);
-        athlete3.setSquatAttempt(215.0, 3, false);
+        athlete3.setSquatAttempt(190.0, 1, judge.validateAttempt());
+        athlete3.setSquatAttempt(205.0, 2, judge.validateAttempt());
+        athlete3.setSquatAttempt(215.0, 3, judge.validateAttempt());
 
-        athlete3.setBenchPress(120.0, 1, true);
-        athlete3.setBenchPress(130.0, 2, true);
-        athlete3.setBenchPress(140.0, 3, true);
+        athlete3.setBenchPress(120.0, 1, judge.validateAttempt());
+        athlete3.setBenchPress(130.0, 2, judge.validateAttempt());
+        athlete3.setBenchPress(140.0, 3, judge.validateAttempt());
 
-        athlete3.setDeadlift(175.0, 1, true);
-        athlete3.setDeadlift(190.0, 2, true);
-        athlete3.setDeadlift(205.0, 3, false);
+        athlete3.setDeadlift(175.0, 1, judge.validateAttempt());
+        athlete3.setDeadlift(190.0, 2, judge.validateAttempt());
+        athlete3.setDeadlift(205.0, 3, judge.validateAttempt());
 
         Tournament tournament = new Tournament("Турнир по тяжёлой атлетике", LocalDate.now(), "Москва");
         tournament.getInfoTournament();
