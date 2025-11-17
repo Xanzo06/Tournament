8a6fe3dc468da26553eeb8a814558699e62cc9f9
CalculatorSpringboot <alfeev01@mail.ru>
2025-11-17 20:33:14 +0300

    Создание Класса Judge

---
+++ src/main/java/org/example/Judge.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import java.util.Random;
+
+public class Judge {
+    String name;
+    int licenseNumber;
+    int experienceYears;
+
+    public Judge(String name, int licenseNumber, int experienceYears) {
+        this.licenseNumber = licenseNumber;
+        this.name = name;
+        this.experienceYears = experienceYears;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLicenseNumber() {
+        return licenseNumber;
+    }
+
+    public void setLicenseNumber(int licenseNumber) {
+        this.licenseNumber = licenseNumber;
+    }
+
+    public int getExperienceYears() {
+        return experienceYears;
+    }
+
+    public void setExperienceYears(int experienceYears) {
+        this.experienceYears = experienceYears;
+    }
+    public boolean validateAttempt(){
+        Random random = new Random();
+        int chance = random.nextInt(100);
+        return chance < 50;
+    }
+}
