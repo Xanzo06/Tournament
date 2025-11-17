266939847c1f487a24fdf83e139dd1c152088c4f
CalculatorSpringboot <alfeev01@mail.ru>
2025-11-17 18:06:50 +0300

    Добавление методов категорий

--- src/main/java/org/example/Tournament.java
+++ src/main/java/org/example/Tournament.java
@@ -1,19 +1,27 @@
 package org.example;
 
+import jdk.jfr.Category;
+
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Tournament {
     private String name;
     private LocalDate date;
     private String location;
     private Athlete[] athletes;
+    private WeightCategory[] weightCategories;
     int count;
+    int categoryCount;
     public Tournament(String name, LocalDate date, String location){
         this.name = name;
         this.date = date;
         this.location = location;
         this.athletes = new Athlete[10];
+        this.weightCategories = new WeightCategory[3];
         this.count = 0;
+        this.categoryCount = 0;
     }
     public void addAthlete(Athlete athlete){
         if (count >= athletes.length){
@@ -25,6 +33,14 @@ public class Tournament {
         athletes[count] = athlete;
         count++;
 
+    }
+    public void addWeightCategory(WeightCategory category){
+        if (categoryCount < weightCategories.length){
+            weightCategories[categoryCount] = category;
+            categoryCount++;
+        }else {
+            System.out.println("Достигнут лимит весовых категорий (" +weightCategories.length + ")");
+        }
     }
     public Athlete findAthleteByName(String name){
         for (Athlete athlete : athletes) {
@@ -49,6 +65,37 @@ public class Tournament {
         System.out.println("Название турнира: " + name + " ; "
                 + "Проводится в городе: " + location +
                 " ; " + "Время проведения: " + date);
+    }
+    public WeightCategory determineCategory(double weight){
+        for (int i = 0; i < categoryCount; i++){
+            WeightCategory weightCategory = weightCategories[i];
+            if (weightCategory.isInCategory(weight)){
+                return weightCategory;
+            }
+        }
+        return null;
+    }
+    public void printResultsByCategory(){
+        for (int i = 0; i < categoryCount; i++){
+            if (weightCategories[i] != null){
+                WeightCategory currentCategory = weightCategories[i];
+                System.out.println("Категория: " + currentCategory);
+
+                List<Athlete> athleteInThisCategory = new ArrayList<>();
+
+                for (int j = 0; j < count; j++) {
+                    Athlete athlete = athletes[j];
+                    if (currentCategory.isInCategory(athlete.getWeight())){
+                        athleteInThisCategory.add(athlete);
+                    }
+                }
+                for (int s = 0; s < athleteInThisCategory.size(); s++){
+                    Athlete athleteInCategory = athleteInThisCategory.get(s);
+                    System.out.println((s + 1) + ". " + athleteInCategory.getName() + "-" + athleteInCategory.getTotalScore());
+                }
+                System.out.println("---");
+            }
+        }
     }
 
 
